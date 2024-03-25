6b1b04105b9c65dfba03aded7ff2f55760f724d1
yukimingo <hakamori5junk.1@gmail.com>
2024-03-25 20:54:34 +0900

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,10 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="css/color.css">
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased background-color">
+        <div class="min-h-screen">
             @include('layouts.announce-header')
             @include('layouts.global-header')
 
@@ -33,5 +34,6 @@
                 {{ $slot }}
             </main>
         </div>
+            @include('layouts.footer')
     </body>
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="bg-gray-800 text-white py-4">
+    <div class="container ms-auto">
+        <div class="grid grid-cols-3 gap-8">
+
+            <div>
+                <h3 class="font-semibold text-lg mb-2">レシピを探す</h3>
+                <ul>
+                    <li class="mb-1"><a href="" class="hover:underline">全てのレシピ</a></li>
+                    <li ><a href="" class="hover:underline">人気のレシピ</a></li>
+                </ul>
+            </div>
+            
+            <div>
+                <h3 class="font-semibold text-lg mb-2">レシピを借りる</h3>
+                <ul>
+                    <li ><a href="" class="hover:underline">レシピの借り方</a></li>
+                </ul>
+            </div>
+            
+            <div>
+                <h3 class="font-semibold text-lg mb-2">お知らせをのページ</h3>
+                <ul>
+                    <li class="mb-1"><a href="" class="hover:underline">ニュース一覧</a></li>
+                    <li ><a href="" class="hover:underline">ログイン</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
